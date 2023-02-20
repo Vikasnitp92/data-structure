ce507cbf25c373e63471fd39555601dcabf260ee
Vikasnitp92 <saranvikaskumar@gmail.com>
2023-02-20 13:13:35 +0530

    second commit

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,4 @@
+
 class Node {
 
     constructor(data){
@@ -12,11 +13,12 @@ class LinkedList{
         this.head=null;
         this.size= 0;
     }
-
+//Inserting the node at the end 
     addAtEnd(data){
     let node = new Node(data);
     if(this.head == null){
         this.head = node;
+       
         return;
     }
     let temp_node= this.head;
@@ -24,8 +26,40 @@ class LinkedList{
       temp_node= temp_node.next;
     }
     temp_node.next = node;
+    this.size++;
     }
+    sizeOflist(){
 
+        return this.size;
+    }
+    insertAt(data, index){
+     if(this.sizeOflist() > (index+1)){
+        return ;
+     }
+     let node = new Node(data);
+
+     if(index == 0){
+        node.next = this.head;
+        this.head = node;
+        return;
+     }
+     let prevNode,currentNode,i;
+     currentNode = this.head;
+      i=0;
+     while (currentNode.next != null ){
+        if(i<index){
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            i++;
+        }
+        if(i== index){
+            node.next=currentNode;
+            prevNode.next = node;
+        }
+     }
+     this.size++;
+    }
+//Prinit the list 
     printlist(){
         let node =this.head;
 
@@ -42,4 +76,6 @@ let li = new LinkedList();
 
 li.addAtEnd(10);
 li.addAtEnd(20);
+li.insertAt(5, 0);
+li.insertAt(30,2)
 li.printlist();
